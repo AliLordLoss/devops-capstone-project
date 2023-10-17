3ec9dd7e2517ea77c06f8c931f1fd2a0957f4c85
AliLordLoss <a.a.ghanati@gmail.com>
2023-10-17 04:41:18 -0400

    - added endpoint to read an account & tests for it

--- service/routes.py
+++ service/routes.py
@@ -69,7 +69,17 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
-
+@app.route("/accounts/<int:id>", methods=["GET"])
+def read_account(id):
+    """
+    Reads an Account
+    This endpoint will read an Account based on the account_id that is passed in the URL param
+    """
+    account = Account.find(id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{id}] cound not be found!")
+        
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,28 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+
+        response = self.client.get(
+            f'{BASE_URL}/{account.id}'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        read_account = response.get_json()
+
+        self.assertEqual(read_account["id"], account.id)
+        self.assertEqual(read_account["name"], account.name)
+        self.assertEqual(read_account["email"], account.email)
+        self.assertEqual(read_account["address"], account.address)
+        self.assertEqual(read_account["phone_number"], account.phone_number)
+        self.assertEqual(read_account["date_joined"], str(account.date_joined))
+
+    def test_account_not_found(self):
+        """It should not Read an Account that does not exist"""
+        response = self.client.get(
+            f'{BASE_URL}/{0}'
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
