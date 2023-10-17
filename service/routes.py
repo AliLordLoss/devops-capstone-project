b9337d8021952ba569674a47418fc4197353a71e
AliLordLoss <a.a.ghanati@gmail.com>
2023-10-17 05:13:15 -0400

    - added endpoint for deleting an account & tests for it

--- service/routes.py
+++ service/routes.py
@@ -107,6 +107,17 @@ def update_account(id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:id>", methods=["DELETE"])
+def delete_account(id):
+    """
+    Deletes an Account
+    This endpoint will delete an Account
+    """
+    account = Account.find(id)
+    if account:
+        account.delete()
+    
+    return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
